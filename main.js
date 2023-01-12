fcee75f157c91bb42c2c9b92cfee73b331c201d3
Jordan D'Souza <jordandsouza352@gmail.com>
2023-01-12 20:38:57 +1300

    added error icon to js css and html

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const form = document.querySelector(".email-form")
 const email = document.querySelector(".email-form__inpt")
 const formbtn = document.querySelector(".email-form__btn")
+const errorMsg = document.querySelector(".email-form__inpt--error");
+const errorIcon = document.querySelector(".error-icon");
 
 formbtn.addEventListener('click', (e)=>{
-  let messages = []
-  console.log(email.validity)
-  if(email.validity.typeMismatch || email.validity.patternMismatch){
-    document.querySelector(".email-form__inpt--error").innerText = "Please provide a valid email";   
+  if(email.validity.typeMismatch || email.validity.patternMismatch || email){
+    e.preventDefault();
+    errorMsg.innerText = "Please provide a valid email"   
+    email.setAttribute("aria-invalid", true);
+    errorIcon.style.display = "block";
+  }else{
+    errorMsg.innerText = ""   
+    email.setAttribute("aria-invalid", false);
+    errorIcon.style.display = "none";
   }
-  // if(email.value === "" || email.value === null){
-  //   message.push("Email is required")
-  // }
-  // if(messages.length > 0){
-  //   e.preventDefault()
-  //   errorElement.innerText = messages.join(", ")
-  // }  
 })
